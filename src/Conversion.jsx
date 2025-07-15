3d411410f8f86a6397f476445aa8af9428944736
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-07-15 21:26:39 +0200

    Create component to try HTML to JSX conversion

---
+++ src/Conversion.jsx
@@ -0,0 +1,23 @@
+function Conversion() {
+  return (
+    <div>
+      <h1>Converting HTML to JSX</h1>
+      <svg>
+        <circle
+          className="circle"
+          cx="25"
+          cy="75"
+          r="20"
+          stroke="green"
+          strokeWidth="2"
+        />
+      </svg>
+      <form>
+        <label for="text">Text </label>
+        <input id="text" type="text" />
+      </form>
+    </div>
+  );
+}
+
+export default Conversion;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
 import { Paragraph } from "./Paragraph.jsx";
+import Conversion from "./Conversion.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <Greeting />
     <Paragraph />
+    <Conversion />
   </StrictMode>
 );
